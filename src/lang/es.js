6a3418408e7ce4edbe721aa693359d383eaf5a5c
Ezequiel Amin <ezequielamin@outlook.com>
2022-10-11 16:33:15 -0300

    added language file and continued setting up the project

--- src/lang/es.js
+++ src/lang/es.js
@@ -1,3 +1,114 @@
-const es = {}
+const es = {
+    LOADING: "Cargando",
+    NO_DATA: "Sin datos",
+    ERROR: "Error",
+
+    DAY: "Día",
+    WEEK: "Semana",
+    MONTH: "Mes",
+    YEAR: "Año",
+
+    WELCOME: "Hola",
+    OVERVIEW: "Resumen",
+    CHARTS: "Gráficos",
+    SETTINGS: "Ajustes",
+    BALANCE: "Saldo",
+    STATE: "Estado",
+
+    LIMIT: "Límite",
+    MONTHLY_LIMIT: "Límite mensual",
+    CURRENT_PERIOD: "Periodo actual",
+    DAYS_PASSED: "Días transcurridos",
+    DAYS_LEFT: "Días restantes",
+
+    SPENT: "Gastado",
+
+    DAILY_AVERAGE: "Promedio diario",
+
+    LEFT_MSG: "De continuar así, llegarás a fin de mes con",
+
+    CATEGORY_CHART: "Gráfico por categoría",
+    DAY_EXPENSES_CHART: "Gráfico de gastos diarios",
+    DAY_INCOMES_CHART: "Gráfico de ingresos diarios",
+    HISTORIC_CHART: "Gráfico histórico",
+
+    TRANSFERS: "Transferencias",
+    EXPENSES: "Gastos",
+    INCOMES: "Ingresos",
+
+    TRANSFER: "Transferencia",
+    EXPENSE: "Gasto",
+    INCOME: "Ingreso",
+
+    MONTHS:[
+        "Enero",
+        "Febrero",
+        "Marzo",
+        "Abril",
+        "Mayo",
+        "Junio",
+        "Julio",
+        "Agosto",
+        "Septiembre",
+        "Octubre",
+        "Noviembre",
+        "Diciembre"
+    ],
+
+    DAYS: [
+        "Domingo",
+        "Lunes",
+        "Martes",
+        "Miércoles",
+        "Jueves",
+        "Viernes",
+        "Sábado"
+    ],
+
+    DAYS_SHORT: [
+        "Dom",
+        "Lun",
+        "Mar",
+        "Mié",
+        "Jue",
+        "Vie",
+        "Sáb"
+    ],
+
+    CATEGORY: "Categoría",
+    AMOUNT: "Cantidad",
+    DATE: "Fecha",
+    DESCRIPTION: "Descripción",
+
+    ADD_TRANSFER: "Añadir transferencia",
+    ADD_EXPENSE: "Añadir gasto",
+    ADD_INCOME: "Añadir ingreso",
+
+    REMOVE_TRANSFER: "Eliminar transferencia",
+    REMOVE_EXPENSE: "Eliminar gasto",
+    REMOVE_INCOME: "Eliminar ingreso",
+
+    EDIT_TRANSFER: "Editar transferencia",
+    EDIT_EXPENSE: "Editar gasto",
+    EDIT_INCOME: "Editar ingreso",
+
+    REMOVE_TRANSFER_CONFIRM: "¿Estás seguro de que quieres eliminar esta transferencia?",
+    REMOVE_EXPENSE_CONFIRM: "¿Estás seguro de que quieres eliminar este gasto?",
+    REMOVE_INCOME_CONFIRM: "¿Estás seguro de que quieres eliminar este ingreso?",
+
+    EDIT_TRANSFER_CONFIRM: "¿Estás seguro de que quieres editar esta transferencia?",
+    EDIT_EXPENSE_CONFIRM: "¿Estás seguro de que quieres editar este gasto?",
+    EDIT_INCOME_CONFIRM: "¿Estás seguro de que quieres editar este ingreso?",
+
+    REMOVE: "Eliminar",
+    EDIT: "Editar",
+
+    CANCEL: "Cancelar",
+    SAVE: "Guardar",
+
+    SETTINGS_LANGUAGE: "Idioma",
+    SETTINGS_CURRENCY: "Moneda",
+    SETTINGS_EXCHANGE: "Intercambio",
+}
 
 export default es;
